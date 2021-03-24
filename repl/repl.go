5c161d1326abcf56da9665b7b9ab9bc96b8e133d
theBashShell <edohbruno@gmail.com>
2021-03-24 16:31:13 +0000

    repl fixed

--- repl/repl.go
+++ repl/repl.go
@@ -1,2 +1,52 @@
 package repl
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os/user"
+
+	"github.com/0xedb/intlang/lexer"
+	"github.com/0xedb/intlang/token"
+)
+
+const (
+	PROMPT        = ">>> "
+	GREET  string = `
+	██╗███╗   ██╗████████╗██╗      █████╗ ███╗   ██╗ ██████╗ 
+	██║████╗  ██║╚══██╔══╝██║     ██╔══██╗████╗  ██║██╔════╝ 
+	██║██╔██╗ ██║   ██║   ██║     ███████║██╔██╗ ██║██║  ███╗
+	██║██║╚██╗██║   ██║   ██║     ██╔══██║██║╚██╗██║██║   ██║
+	██║██║ ╚████║   ██║   ███████╗██║  ██║██║ ╚████║╚██████╔╝
+	╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+																 
+	`
+)
+
+func StartREPL(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	fmt.Println(GREET)
+	user, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Hello, ", user.Name)
+	fmt.Println("Welcome to the intLANG programming language")
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Token != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}

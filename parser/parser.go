322e1f4d1d7247bde22802978fedc9d934c71508
theBashShell <edohbruno@gmail.com>
2021-03-25 19:02:22 +0000

    boolean expression parser

--- parser/parser.go
+++ parser/parser.go
@@ -35,6 +35,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegralLiteral)
 	p.registerPrefix(token.NOT, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -46,6 +48,10 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.GRT, p.parseInfixExpression)
 
 	return p
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.cur, Value: p.curTokenIs(token.TRUE)}
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {

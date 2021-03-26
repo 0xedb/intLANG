cb4d0b93508d96b895d79866210094a3df402ddf
theBashShell <edohbruno@gmail.com>
2021-03-26 22:41:03 +0000

    call expressions parsing

--- parser/parser.go
+++ parser/parser.go
@@ -40,6 +40,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.IF, p.parseIfExpression)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -51,6 +52,31 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.GRT, p.parseInfixExpression)
 
 	return p
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.cur, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+	p.nextToken()
+	args = append(args, p.parseExpression(token.LOWEST))
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(token.LOWEST))
+	}
+	if !p.expectToken(token.RPAREN) {
+		return nil
+	}
+	return args
 }
 
 func (p *Parser) parseFunctionLiteral() ast.Expression {
